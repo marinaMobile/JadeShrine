ec7ca4a290c08ff429d430e1b2a41064cb265a51
Marina <marina@Marinas-MacBook-Pro.local>
2022-11-03 14:09:05 +0300

    first commit

---
+++ app/src/main/java/com/tocaboca/to/black/CNST.kt
@@ -0,0 +1,17 @@
+package com.tocaboca.to.black
+
+object CNST {
+
+    const val AF_DEV_KEY = "KNNiwvQWRCdW6x9NhNVwmY"
+    const val jsoupCheck = "5h7v"
+    const val ONESIGNAL_APP_ID = "b0fd731d-b952-4e25-bb71-23269f322513"
+
+    var lru = "http://jadeshrine.xyz/go.php?to=1&"
+
+
+    var odone = "sub_id_1="
+
+    var MAIN_ID: String? = ""
+    var C1: String? = "c11"
+    var D1: String? = "d11"
+}
